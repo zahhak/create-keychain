514e121e01264c3d0622052d83caab5a333560b4
Ilian Iliev <ilianiliev@Ilians-MacBook-Air.local>
2014-10-26 12:26:15 +0200

    Initial Commit

---
+++ create-keychain/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  create-keychain
+//
+//  Created by Ilian Iliev on 10/26/14.
+//  Copyright (c) 2014 Ilian Iliev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
